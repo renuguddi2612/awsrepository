36d16d4edd330ee204019589e8e73ab5511686a3
renuguddi2612 <renu.guddi0626@gmail.com>
2026-03-11 21:04:40 +0530

    first commit

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
 
     s3 = boto3.client('s3')
 
-    bucket_name = "my-lambda-bucket-123456"
+    bucket_name = "my-lambda-bucket"
 
     response = s3.create_bucket(
         Bucket=bucket_name,
